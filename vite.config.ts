a2e10385944958d159a09323de1ed561c7804316
furugomu <furugomu@gmail.com>
2026-03-30 13:43:58 +0900

    dekita

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
+  server: { proxy: { "/api": "http://localhost:8787" } },
   staged: {
     "*": "vp check --fix",
   },
